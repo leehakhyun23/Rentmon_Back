f4bff1fc6a47b073998b6da68cc44b38196c0123
jungjoo-lee <77317343+jungjoo-lee@users.noreply.github.com>
2024-08-06 10:45:13 +0900

    Feat entity 설계
    너무 많아서 각자 만듭시다....

--- src/main/java/com/himedia/rentmon_back/entity/Space.java
+++ src/main/java/com/himedia/rentmon_back/entity/Space.java
@@ -1,20 +1,36 @@
 package com.himedia.rentmon_back.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
+
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "Space")
 @Getter
 public class Space {
-    private int sseq;
-    private String hostid;
-    private int mc_num;
-    private int f_num;
-    private int price;
-    private String title;
-    private String subTitle;
-    private String content;
-    private 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sseq")
+    private int sseq;               // 기본키
+    private String hostid;          // 호스트아이디
+    private int c_num;              // 분류 아이디
+    private int f_num;              // 요금제 아이디
+    private int price;              // 가격
+    private String title;           // 제목
+    private String subTitle;        // 부제
+    private String content;         // 내용
+    private Timestamp startTime;    // 이용시작 시간
+    private Timestamp endTime;      // 마감시간
+    private String closed;          // 휴일
+    private String caution;         // 주의사항
+    private String zipCode;         // 우편번호
+    private String province;        // 시도군
+    private String town;            // 구읍?
+    private String village;         // 동면리
+    private String address_detail;  // 상세주소
+    private int min_time;           // 최소이용시간
+    private int personnal;          // 인원
+    private int max_personnal;      // 최대인원
+    private Timestamp created_at;   // 등록일
 }
