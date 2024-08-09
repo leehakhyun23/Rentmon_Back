f0aa8e7b209a002025bc95be207df1ef61923daa
Joungbin Park <163955135+JoungbinPark@users.noreply.github.com>
2024-08-09 17:42:10 +0900

    SpaceDTO InnerClass 삭제

---
+++ src/main/java/com/himedia/rentmon_back/repository/ReviewRepository.java
@@ -0,0 +1,12 @@
+package com.himedia.rentmon_back.repository;
+
+import com.himedia.rentmon_back.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Reservation, Integer> {
+    ArrayList findBySseq(int sseq);
+}
